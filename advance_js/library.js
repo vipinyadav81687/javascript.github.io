4a36b3df10c9fba4b477522de15cfb1602c7f9ae
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-07-02 21:07:41 +0530

    2July2026

---
+++ advance_js/library.js
@@ -0,0 +1,15 @@
+export default function(){
+    console.log("Hello");
+}
+
+// export let message = "ES6 Modules";
+// export function user(name){
+//     console.log(`Hello ${name}`)
+// }
+
+// export class test{
+//     constructor(){
+//         console.log("constructor Method");
+        
+//     }
+// }
---
+++ advance_js/main.js
@@ -0,0 +1,9 @@
+import { default as vipin} from "./library.js";
+
+vipin();
+// import {message,user,test} from "./library.js";
+//import * as vip from "./library.js";
+
+// console.log(vip.message);
+// vip.user("vipin");
+// let a = new vip.test();
